4643a694c0175a2a0e8e14ad26ed7376d213138d
JackelinPerez-username <filomenochahua@gmail.com>
2018-11-26 09:51:46 -0500

    depurar

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ const menuPrincipal= () =>{
         valor_encrip_desc=listenCode(saveClave_.value,parseInt(saveOffset_.value));
       else
         valor_encrip_desc=listenDecode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-      console.log('valor return= '+valor_encrip_desc);
-      outTextEncripDesencript.innerHTML=valor_encrip_desc;
+      
+	  outTextEncripDesencript.innerHTML=valor_encrip_desc;
     }
   );
   saveActionButonDesEncript.addEventListener('click',
@@ -53,8 +53,8 @@ const menuPrincipal= () =>{
         valor_encrip_desc=listenDecode(saveClave_.value,parseInt(saveOffset_.value));
       else
         valor_encrip_desc=listenCode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-      console.log('valor return= '+valor_encrip_desc);
-      outTextEncripDesencript.innerHTML=valor_encrip_desc;
+
+	  outTextEncripDesencript.innerHTML=valor_encrip_desc;
     }
   );
 }
