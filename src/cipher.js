fa280102fd17238eea1282adff8b23b60e908313
JackelinPerez-username <filomenochahua@gmail.com>
2018-11-26 11:21:12 -0500

    r

--- src/cipher.js
+++ src/cipher.js
@@ -33,10 +33,10 @@ let Decode_ = (saveClave,saveOffset) =>{
 		if(saveClave[i].charCodeAt(0)>=97){
 			if(Math.sign((saveClave[i].charCodeAt(0)-97-saveOffset)%26)>=0)/*Se considero '==' ya que (-num)%26 ===-0*/
 				encriptClaveAux.push((saveClave[i].charCodeAt(0)-97-saveOffset)%26+97);
-  			else
+			else
 				encriptClaveAux.push(26-(Math.abs(saveClave[i].charCodeAt(0)-97-saveOffset)%26)+97);
 		}
-  		else{
+		else{
 			if(Math.sign((saveClave[i].charCodeAt(0)-65- saveOffset)%26)>=0)
 				encriptClaveAux.push((saveClave[i].charCodeAt(0)-65- saveOffset)%26+65);
 			else
