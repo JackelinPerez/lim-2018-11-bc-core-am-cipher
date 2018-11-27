8d698e04529797efd53bd8bddce538d8cd01a686
JackelinPerez-username <filomenochahua@gmail.com>
2018-11-27 18:15:35 -0500

    ccs

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ let saveActionButonOK;
 let saveActionButonEncript;
 let saveActionButonDesEncript;
 let outTextEncripDesencript;
-let saveClave_;
-let saveOffset_;
+let saveClave_='';
+let saveOffset_='';
 
 const listenDecode = (saveClave__,saveOffset__) =>{
   return cipher.decode(saveClave__,saveOffset__);
@@ -26,7 +26,7 @@ const menuPrincipal= () =>{
   saveOffset_=document.getElementById('texto2');
 
   document.getElementById("screen2").style.display="none";
-  
+
   saveActionButonOK.addEventListener('click',
     function(){
       document.getElementById("screen1").style.display="none";
@@ -35,20 +35,32 @@ const menuPrincipal= () =>{
   )
   saveActionButonEncript.addEventListener('click',
     function(){
-      if(Math.sign(parseInt(saveOffset_.value))>=0)
-        valor_encrip_desc=listenCode(saveClave_.value,parseInt(saveOffset_.value));
-      else
-        valor_encrip_desc=listenDecode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-	outTextEncripDesencript.innerHTML=valor_encrip_desc;
+      if(saveClave_.value.length!==0 && saveOffset_.value.length!==0){
+        if(Math.sign(parseInt(saveOffset_.value))>=0)
+          valor_encrip_desc=listenCode(saveClave_.value,parseInt(saveOffset_.value));
+        else
+          valor_encrip_desc=listenDecode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
+        outTextEncripDesencript.innerHTML=valor_encrip_desc;
+      }
+      else {
+        if(saveClave_.value.length===0) alert('Por favor ingrese texto');
+        else alert('Por favor ingrese offset');
+      }
     }
   );
   saveActionButonDesEncript.addEventListener('click',
     function(){
-      if(Math.sign(parseInt(saveOffset_.value))>=0)
-        valor_encrip_desc=listenDecode(saveClave_.value,parseInt(saveOffset_.value));
-      else
-        valor_encrip_desc=listenCode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-	outTextEncripDesencript.innerHTML=valor_encrip_desc;
+      if(saveClave_.value.length!==0 && saveOffset_.value.length!==0){
+        if(Math.sign(parseInt(saveOffset_.value))>=0)
+          valor_encrip_desc=listenDecode(saveClave_.value,parseInt(saveOffset_.value));
+        else
+          valor_encrip_desc=listenCode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
+        outTextEncripDesencript.innerHTML=valor_encrip_desc;
+      }
+      else{
+        if(saveClave_.value.length===0) alert('Por favor ingrese texto');
+        else alert('Por favor ingrese offset');
+      }
     }
   );
 }
