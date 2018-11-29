e3f3dad3e0083ae0fce6765c0246d37cc88c51e4
JackelinPerez-username <filomenochahua@gmail.com>
2018-11-29 16:17:55 -0500

    Readme, incluir archivos .txt, css

--- src/index.js
+++ src/index.js
@@ -1,68 +1,93 @@
-//variables globales
-
 let saveActionButonOK;
 let saveActionButonEncript;
 let saveActionButonDesEncript;
 let outTextEncripDesencript;
-let saveClave_='';
-let saveOffset_='';
+let saveFile;
+let saveClave = '';
+let saveOffset = '';
 
-const listenDecode = (saveClave__,saveOffset__) =>{
-  return cipher.decode(saveClave__,saveOffset__);
+const listenDecode = (saveClave_, saveOffset_) => {
+  return cipher.decode(saveClave_, saveOffset_);
 }
 
-const listenCode = (saveClave__,saveOffset__) =>{
-  return cipher.encode(saveClave__,saveOffset__);
+const listenCode = (saveClave_, saveOffset_) => {
+  return cipher.encode(saveClave_, saveOffset_);
 }
 
-const menuPrincipal= () =>{
-  let valor_encrip_desc='';
-  saveActionButonOK=document.getElementById('button1');
-  saveActionButonEncript=document.getElementById('button2');
-  saveActionButonDesEncript=document.getElementById('button3');
-  outTextEncripDesencript=document.getElementById('result');
+const menuPrincipal = () => {
+  let valueEncripDescrip = '';
+  saveActionButonOK = document.getElementById('button1');
+  saveActionButonEncript = document.getElementById('button2');
+  saveActionButonDesEncript = document.getElementById('button3');
+  outTextEncripDesencript = document.getElementById('result');
 
-  saveClave_=document.getElementById('texto1');
-  saveOffset_=document.getElementById('texto2');
+  saveFile = document.getElementById('saveFile');
+  saveClave = document.getElementById('texto1');
+  saveOffset = document.getElementById('texto2');
 
-  document.getElementById("screen2").style.display="none";
+  document.getElementById('screen2').style.display = 'none';
+
+  saveClave.addEventListener('click',function(){
+    if(saveClave.value.length!==0) outTextEncripDesencript.innerHTML='';
+  });
+
+  saveOffset.addEventListener('click',function(){
+    if(saveClave.value.length!==0) outTextEncripDesencript.innerHTML='';
+  });
 
   saveActionButonOK.addEventListener('click',
-    function(){
-      document.getElementById("screen1").style.display="none";
-      document.getElementById("screen2").style.display="block";
+    function () {
+      document.getElementById('screen1').style.display = 'none';
+      document.getElementById('screen2').style.display = 'block';
     }
   )
   saveActionButonEncript.addEventListener('click',
-    function(){
-      if(saveClave_.value.length!==0 && saveOffset_.value.length!==0){
-        if(Math.sign(parseInt(saveOffset_.value))>=0)
-          valor_encrip_desc=listenCode(saveClave_.value,parseInt(saveOffset_.value));
+    function () {
+      if (saveClave.value.length !== 0 && saveOffset.value.length !== 0) {
+        if (Math.sign(parseInt(saveOffset.value)) >= 0)
+          valueEncripDescrip = listenCode(saveClave.value, parseInt(saveOffset.value));
         else
-          valor_encrip_desc=listenDecode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-        outTextEncripDesencript.innerHTML=valor_encrip_desc;
+          valueEncripDescrip = listenDecode(saveClave.value, Math.abs(parseInt(saveOffset.value)));
+        outTextEncripDesencript.innerHTML = valueEncripDescrip;
       }
       else {
-        if(saveClave_.value.length===0) alert('Por favor ingrese texto');
+        if (saveClave.value.length === 0) alert('Por favor ingrese texto');
         else alert('Por favor ingrese offset');
       }
     }
   );
   saveActionButonDesEncript.addEventListener('click',
-    function(){
-      if(saveClave_.value.length!==0 && saveOffset_.value.length!==0){
-        if(Math.sign(parseInt(saveOffset_.value))>=0)
-          valor_encrip_desc=listenDecode(saveClave_.value,parseInt(saveOffset_.value));
+    function () {
+      if (saveClave.value.length!==0 && saveOffset.value.length !== 0){
+        if (Math.sign(parseInt(saveOffset.value)) >= 0)
+          valueEncripDescrip = listenDecode(saveClave.value, parseInt(saveOffset.value));
         else
-          valor_encrip_desc=listenCode(saveClave_.value,Math.abs(parseInt(saveOffset_.value)));
-        outTextEncripDesencript.innerHTML=valor_encrip_desc;
+          valueEncripDescrip = listenCode(saveClave.value, Math.abs(parseInt(saveOffset.value)));
+        outTextEncripDesencript.innerHTML = valueEncripDescrip;
       }
-      else{
-        if(saveClave_.value.length===0) alert('Por favor ingrese texto');
+      else {
+        if (saveClave.value.length === 0) alert('Por favor ingrese texto');
         else alert('Por favor ingrese offset');
       }
     }
   );
+
+
+  let saveFileFunction= (event)=>{
+    let input=event.target;
+    let readFile= new FileReader();
+    readFile.onload= function(){
+      let textRead=readFile.result;
+      document.getElementById('texto1').value = textRead;
+    }
+    readFile.readAsText(input.files[0]);
+  }
+
+  saveFile.onchange = function(){
+    if(saveClave.value.length!==0) outTextEncripDesencript.innerHTML='';
+    saveFileFunction(event);
+  };
+
 }
 
 menuPrincipal();
